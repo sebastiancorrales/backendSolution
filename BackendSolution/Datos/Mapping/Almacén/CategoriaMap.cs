027a9bd648f1076ec2f4e6be92acd016205823e1
sebastiancorrales <50119107+sebastiancorrales@users.noreply.github.com>
2019-10-03 15:28:04 -0500

    Add files via upload

---
+++ BackendSolution/Datos/Mapping/Almacén/CategoriaMap.cs
@@ -0,0 +1,25 @@
+﻿using Entidades.Almacén;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datos.Mapping.Almacén
+{
+    public class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        //metodo que no retorna debido a que tiene la palabra void
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("categoria")
+                .HasKey(c => c.idcategoria);
+            builder.Property(c => c.nombre)
+                .HasMaxLength(50);
+            builder.Property(c => c.descripcion)
+                .HasMaxLength(256);
+            builder.Property(c => c.condicion);
+            
+        }
+    }
+}
---
+++ BackendSolution/Entidades/Almacén/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entidades.Almacén
+{
+    public class Categoria
+    {
+        //get es para mostrar el valor del atributo
+        //set es para modificar el valor del atributo
+        public int idcategoria { get; set; }
+        //Las validaciones anteceden el campo
+        [Required]
+        [StringLength(50, MinimumLength =3,ErrorMessage = "Cantidad de caracteres superior o inferior a los recomendado")]
+        public string nombre { get; set; }
+        [StringLength(256, MinimumLength = 2, ErrorMessage = "Cantidad de caracteres superior o inferior a los recomendado")]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
